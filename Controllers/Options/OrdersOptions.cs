3bf46cb0092bd0a9c57dc6beac188835320dcf3b
Boris Wang <bwang@nextestate.com>
2017-08-03 21:06:13 +0800

    Remove ToString

--- Controllers/Options/OrdersOptions.cs
+++ Controllers/Options/OrdersOptions.cs
@@ -7,10 +7,5 @@ namespace MyWebApi.Controllers.Options
             ConnectionString = "mongodb://localhost:27017";
         }
         public string ConnectionString { get; set; }
-
-        public override string ToString()
-        {
-            return string.Format("ConnectionString: {0}", ConnectionString);
-        }
     }
 }
